af45de02d749ace56453e7f2dd249d5ceb817058
Mahesh Patade <patadeahesh@gmail.com>
2015-10-10 12:08:13 +0530

    Added queue table and new structure

--- transcoding.sql
+++ transcoding.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.7
+-- version 3.5.7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 18, 2015 at 11:54 AM
+-- Generation Time: Oct 10, 2015 at 12:46 AM
 -- Server version: 5.6.21-70.1
 -- PHP Version: 5.3.3
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `transcoding`
 --
-CREATE DATABASE IF NOT EXISTS `transcoding` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `transcoding`;
 
 -- --------------------------------------------------------
 
@@ -28,6 +26,7 @@ USE `transcoding`;
 -- Table structure for table `cpdetails`
 --
 
+DROP TABLE IF EXISTS `cpdetails`;
 CREATE TABLE IF NOT EXISTS `cpdetails` (
   `cp` varchar(100) NOT NULL,
   `user` varchar(100) NOT NULL,
@@ -42,24 +41,40 @@ CREATE TABLE IF NOT EXISTS `cpdetails` (
 -- Table structure for table `jobs`
 --
 
+DROP TABLE IF EXISTS `jobs`;
 CREATE TABLE IF NOT EXISTS `jobs` (
-  `job_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `path` varchar(255) NOT NULL,
+  `jobid` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) NOT NULL,
+  `filepath` varchar(255) NOT NULL,
   `duration` int(11) NOT NULL,
-  `node_status` int(11) NOT NULL,
-  `no_nodes` int(11) NOT NULL,
-  `job_status` int(11) NOT NULL,
-  `node_failed` varchar(255) DEFAULT NULL,
-  `job_failed` varchar(255) DEFAULT NULL,
-  `start_time` varchar(100) NOT NULL,
-  `conversion_end_time` varchar(100) DEFAULT NULL,
-  `end_time` varchar(100) DEFAULT NULL,
-  `node_id` varchar(10) NOT NULL,
-  `cp` varchar(50) DEFAULT NULL,
-  `curmaster` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`job_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5959 ;
+  `nodecount` int(11) NOT NULL,
+  `jobcount` int(11) NOT NULL DEFAULT '0',
+  `jobcomplete` int(11) NOT NULL,
+  `error` varchar(255) DEFAULT NULL,
+  `starttime` varchar(100) DEFAULT NULL,
+  `conversiontime` varchar(100) DEFAULT NULL,
+  `totaltime` varchar(100) DEFAULT NULL,
+  `product` varchar(50) DEFAULT NULL,
+  `master` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`jobid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=58 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queue`
+--
+
+DROP TABLE IF EXISTS `queue`;
+CREATE TABLE IF NOT EXISTS `queue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `jobid` int(11) NOT NULL,
+  `starttime` varchar(50) NOT NULL,
+  `length` varchar(50) NOT NULL,
+  `node` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=222 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
